0a63755946b3e87fd067fc23dab4092b70588ffc
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-02-04 04:13:09 -0300

    Criado rotas reaproveitaveis

--- src/lib/auth.module.ts
+++ src/lib/auth.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { SecureInnerPagesGuard } from './secure-inner-pages.guard';
+import { AuthGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
+import { AuthRoutingModule } from './auth.routing';
 
 @NgModule({
   declarations: [
@@ -15,9 +20,11 @@ import { CommonModule } from '@angular/common';
     ForgotPasswordComponent, 
     VerifyEmailComponent],
   imports: [
+    AuthRoutingModule,
     CommonModule,
     AngularFireAuthModule
-  ],
-  exports: []
+  ],  
+  exports: [AuthRoutingModule]
+
 })
 export class AuthModule { }
